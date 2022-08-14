9a3ee951d6d321857949d6491c75abd9253aa79a
Matheus <matheusmsg87@gmail.com>
2022-08-14 15:19:12 -0300

    atualização projeto1

--- Projeto1/jogodaforca/src/App.jsx
+++ Projeto1/jogodaforca/src/App.jsx
@@ -23,10 +23,46 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
 
-  const [words] =useState(wordsList)
+  const [words] = useState(wordsList)
+
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
 
   //Iniciando o jogo
+
+  const pickWordAndCategory = () => {
+    //pick a random category
+    const categories = Object.keys(words)
+
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    console.log(category)
+
+    // pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+    console.log(word)
+
+    return {word, category}
+  }
+
   const startGame = () => {
+    //pick word and pick category
+    const {word, category} = pickWordAndCategory()
+
+    // create an array of letters
+    let wordLetters = word.split("")
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+    console.log(wordLetters)
+
+    console.log(word, category)
+
+    //fill states
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters)
+
     setGameStage(stages[1].name)
   }
   
