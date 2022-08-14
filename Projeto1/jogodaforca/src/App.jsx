0c6baf2e1868b7ac067b22962f0fd7e2352b2d50
Matheus <matheusmsg87@gmail.com>
2022-08-14 13:07:40 -0300

    atualização projeto1

--- Projeto1/jogodaforca/src/App.jsx
+++ Projeto1/jogodaforca/src/App.jsx
@@ -18,18 +18,33 @@ const stages = [
   {id: 3, name: "end"},
 ]
 
+
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
 
   const [words] =useState(wordsList)
 
-  console.log(words)
+  //Iniciando o jogo
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+  
+  //processando a letra
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  //resetando o jogo
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
 
   return (
     <div className="App">
-      { gameStage === 'start' && <StartJogo />}
-      { gameStage === 'game' && <Game />}
-      { gameStage === 'end' && <GameOver />}
+      { gameStage === 'start' && <StartJogo startGame={startGame} />}
+      { gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      { gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   )
 }
--- Projeto1/jogodaforca/src/components/StartJogo.jsx
+++ Projeto1/jogodaforca/src/components/StartJogo.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./StartJogo.css"
 
-function StartJogo() {
+function StartJogo({startGame}) {
   return (
     <div className='start'>
         <h1>Secret Word</h1>
         <p>Clique no butão para começar a jogar</p>
-        <button>Começar o jogo</button>
+        <button onClick={startGame}>Começar o jogo</button>
     </div>
   )
 }
