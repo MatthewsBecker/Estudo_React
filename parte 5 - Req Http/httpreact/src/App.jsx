e62c8501aec038fcff04f06bfdd4b558d70a82bd
Matheus <matheusmsg87@gmail.com>
2022-08-15 20:04:54 -0300

    parte 5 http react

---
+++ parte 5 - Req Http/httpreact/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect} from 'react'
+import './App.css'
+
+const url = "http://localhost:3000/products"
+
+function App() {
+  const [products, setProducts] = useState([])
+
+  // Resgatando dados
+  useEffect(() => {
+
+    async function fetchData(){
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      setProducts(data)
+    }
+
+    fetchData()
+  }, [])
+
+  console.log(products)
+
+  return (
+    <div className="App">
+      <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
